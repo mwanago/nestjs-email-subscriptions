24bd0708040c0ae5af5aa9bbadf16164e4631b3d
marcin <wanago.marcin@gmail.com>
2020-11-29 19:06:18 +0100

    feat(gRPC): use GRPC_CONNECTION_URL from .env

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,18 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { join } from 'path';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
 
   await app.connectMicroservice<MicroserviceOptions>({
     transport: Transport.GRPC,
     options: {
       package: 'subscribers',
-      protoPath: join(process.cwd(), 'src/subscribers/subscribers.proto')
+      protoPath: join(process.cwd(), 'src/subscribers/subscribers.proto'),
+      url: configService.get('GRPC_CONNECTION_URL')
     },
   });
 
